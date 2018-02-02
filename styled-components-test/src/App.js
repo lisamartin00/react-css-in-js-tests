2fad84053fdf515b01170fe0571e8f6ca8502840
Lisa Martin <lmartin@tangogroup.com>
2018-02-02 14:20:21 -0700

    Add variable experiment

--- styled-components-test/src/App.js
+++ styled-components-test/src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 import ImageCard from './image-card/image-card';
+import styled from "styled-components";
+
+const containerWidth = '1160px';
+
+const Container = styled.div`
+  margin: 40px auto;
+  width: ${containerWidth};
+  
+  @media (max-width: 1160px) {
+    width: 100%;
+  }
+}
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App container">
-        <h1>styled components</h1>
-        <div className="grid">
-          <ImageCard src='https://picsum.photos/288/160?image=1080' title='Stop!  Collaborate, and listen.' />
-          <ImageCard src='https://picsum.photos/288/160/?image=1081' title='Ice is back with my brand new invention.' />
-          <ImageCard src='https://picsum.photos/288/160/?image=1082' title='Something grabs ahold of me tightly.' />
-        </div>
+      <div className="App">
+        <Container>
+            <h1>styled components</h1>
+            <div className="grid">
+              <ImageCard src='https://picsum.photos/288/160?image=1080' title='Stop!  Collaborate, and listen.' />
+              <ImageCard src='https://picsum.photos/288/160/?image=1081' title='Ice is back with my brand new invention.' />
+              <ImageCard src='https://picsum.photos/288/160/?image=1082' title='Something grabs ahold of me tightly.' />
+            </div>
+          </Container>
       </div>
     );
   }
